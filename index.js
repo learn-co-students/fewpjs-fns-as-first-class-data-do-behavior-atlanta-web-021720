38640927d523a76eeb76bae4d70b717745e6b728
gcampbell391 <gcampbell391@gmail.com>
2020-03-31 18:47:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+
+    let newTime = parseInt(time)
+    if (newTime > 17) {
+        return "Good Evening"
+    }
+    if (newTime < 12) {
+        return "Good Morning"
+    }
+    if (12 < newTime < 17) {
+        return "Good Afternoon"
+    }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(time) {
+    document.querySelector("#greeting").innerText = time
+}
